424af32f8006cb205a9f3055a5d6c3c857f4f737
Christian Henry <christianchinonso000@gmail.com>
2025-10-06 19:10:57 +0100

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -80,7 +80,7 @@ class Product(models.Model):
     def get_original_naira_price(self):
         """Convert original USD price to Naira"""
         exchange_rate = 1500  # Adjust as needed
-        return float(self.price) * exchange_rate
+        return float(self.price) / exchange_rate
     
     def __str__(self):
         return self.name
